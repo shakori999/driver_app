1b51835639b716eb65bb56d230917ccc49a68f74
shakori999 <shakori999@gmail.com>
2021-12-29 14:18:47 +0300

    sending & receiving broadcast messages

--- taxi/trips/consumers.py
+++ taxi/trips/consumers.py
@@ -2,10 +2,20 @@ from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
+    groups = ['test']
+
     async def connect(self):
+        await self.channel_layer.group_add(
+            group='test',
+            channel=self.channel_name
+        )
         await self.accept()
 
     async def disconnect(self, code):
+        await self.channel_layer.group_discard(
+            group='test',
+            channel=self.channel_name
+        )
         await super().disconnect(code)
 
     async def receive_json(self, content, **kwargs):
@@ -15,3 +25,9 @@ class TaxiConsumer(AsyncJsonWebsocketConsumer):
                 'type': message_type,
                 'data':content.get('data'),
             })
+
+    async def echo_message(self, message):
+        await self.send_json({
+            'type': message.get('type'),
+            'data': message.get('data'),
+        })
--- taxi/trips/tests/test_websocket.py
+++ taxi/trips/tests/test_websocket.py
@@ -1,3 +1,4 @@
+import channels
 from channels.testing import WebsocketCommunicator
 from django.http import response
 from channels.layers import get_channel_layer
@@ -36,6 +37,23 @@ class TestWebSocket:
             'data': 'This is a test message.',
         }
         await communicator.send_json_to(message)
+        response = await communicator.receive_json_from()
+        assert response == message
+        await communicator.disconnect()
+
+    async def test_can_send_and_receive_broadcast_messages(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path='/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        message = {
+            'type': 'echo.message',
+            'data': 'This is a test message',
+        }
+        channel_layer = get_channel_layer()
+        await channel_layer.group_send('test', message=message)
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()
