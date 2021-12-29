0f2f23ce580f911b378933620798baab09786b26
shakori999 <shakori999@gmail.com>
2021-12-29 13:15:44 +0300

    connecting to the server

--- taxi/taxi/routing.py
+++ taxi/taxi/routing.py
@@ -1,8 +1,14 @@
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
 
-application = ProtocolTypeRouter(
-    {
-        'http': get_asgi_application(),
-    }
-)
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from trips.consumers import TaxiConsumer
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    # new
+    'websocket': URLRouter([
+        path('taxi/', TaxiConsumer.as_asgi()),
+    ]),
+})
